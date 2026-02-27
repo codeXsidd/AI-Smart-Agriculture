812fe8255793077ed16d0be3eec1bdcc04c2fd5a
Siddharth S <179855721+codeXsidd@users.noreply.github.com>
2026-02-27 13:28:28 +0530

    Create cure_dict.py

---
+++ backend/cure_dict.py
@@ -0,0 +1,10 @@
+cure_dict = {
+    "Tomato Late Blight": {
+        "organic": "Neem oil spray every 7 days",
+        "chemical": "Apply Mancozeb fungicide"
+    },
+    "Healthy": {
+        "organic": "No treatment required",
+        "chemical": "No treatment required"
+    }
+}
