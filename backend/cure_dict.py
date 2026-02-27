310385bdc2433216bce3964ef009548df724abb6
Siddharth S <179855721+codeXsidd@users.noreply.github.com>
2026-02-27 13:58:53 +0530

    Update cure_dict.py

--- backend/cure_dict.py
+++ backend/cure_dict.py
@@ -1,10 +1,14 @@
 cure_dict = {
-    "Tomato Late Blight": {
-        "organic": "Neem oil spray every 7 days",
-        "chemical": "Apply Mancozeb fungicide"
-    },
     "Healthy": {
-        "organic": "No treatment required",
-        "chemical": "No treatment required"
+        "organic": "No treatment required.",
+        "chemical": "No chemical treatment required."
+    },
+    "Tomato Late Blight": {
+        "organic": "Spray neem oil every 7 days.",
+        "chemical": "Apply Mancozeb fungicide."
+    },
+    "Tomato Early Blight": {
+        "organic": "Use compost tea spray.",
+        "chemical": "Apply Chlorothalonil fungicide."
     }
 }
